af6c3b24a15fed5e5a208aeb29df8e18d50cef06
Rashmi-278 <rashmiabbigeri@gmail.com>
2023-07-23 22:38:26 +0530

    swagger bind

--- swagger/swaggerAPI.py
+++ swagger/swaggerAPI.py
@@ -454,4 +454,4 @@ def is_token_used():
 #handle with try/catch data being sent.
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
